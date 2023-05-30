bf565b04f65e8923348c9447ca9eebece20c2283
Andy Jacobsen <atze.danjo@gmail.com>
2023-05-30 20:05:33 +0000

    Style the front page

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,10 @@ from django.forms import ModelForm
 from .models import URL
 
 class URLForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['long_url'].widget.attrs.update({'class': 'form-control'})
+    
     class Meta:
         model = URL
         fields = ('long_url',)
